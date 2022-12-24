cbc298d6d235ec2ff8c5be7088e2cf3de4148ffb
Mauri <mauriciovera272@gmail.com>
2022-12-23 22:36:22 -0300

    Se le cambio el nombre de Conversor logia a ConversorEjecutable

---
+++ src/com/conversor/ConversorEjecutable.java
@@ -0,0 +1,13 @@
+package com.conversor;
+
+
+import com.gui.Ventaana;
+
+public class ConversorEjecutable {
+	public static void main(String[] args) {
+		Ventaana ventana =  new Ventaana();
+		ventana.setVisible(true);
+		ventana.setLocationRelativeTo(null);
+		
+	}
+}

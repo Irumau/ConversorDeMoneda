3915cc80633a4eb69eb6c5872c14531b8d1f9dcc
Mauri <mauriciovera272@gmail.com>
2022-12-23 22:37:30 -0300

    Carpeta donde se realizaron las pruebas para utilizar la API

--- src/com/conexion/ConexionApi.java
+++ src/com/conexion/ConexionApi.java
@@ -2,34 +2,128 @@ package com.conexion;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
+
 import java.util.Scanner;
+import org.json.JSONObject;
+
+
 
 public class ConexionApi {
-	public static void main(String[] args) {
+
+	public enum ConvertCoin {
+		USD, EUR, COP, BTC, ARS, MXN, CLP, PEN, CRC, BRL, DOP, SVC, BOB
+	}
+
+	public static Double convertConexion(String from, String to, Double amount) {
+
 		try {
-			URL url = new URL("https://api.getgeoapi.com/v2/currency/convert?api_key=02d64c36ab1702f7c2a39d8001dea0017409f3ae");
+			URL url = new URL(
+					"https://api.getgeoapi.com/v2/currency/convert?api_key=02d64c36ab1702f7c2a39d8001dea0017409f3ae&from="
+							+ from + "&to=" + to + "&amount=" + amount + "&format=json");
+
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
 			conn.setRequestMethod("GET");
 			conn.connect();
 			int responseCode = conn.getResponseCode();
-			
-			if(responseCode != 200) {
-					throw new RuntimeException("Ocurrio un error" + responseCode);
-			}else {
+
+			if (responseCode != 200) {
+				throw new RuntimeException("Ocurrio un error " + responseCode);
+			} else {
 				StringBuilder informationString = new StringBuilder();
 				Scanner scanner = new Scanner(url.openStream());
-				
-				while(scanner.hasNext()) {
+
+				while (scanner.hasNext()) {
 					informationString.append(scanner.nextLine());
 				}
+
 				scanner.close();
+				//System.out.print(informationString);
+				JSONObject jsonObject = new JSONObject(informationString.toString());
 				
-				System.out.println(informationString);
+				double exchangeRate = jsonObject.getJSONObject("rates").getJSONObject(to).getDouble("rate");
 			}
 			
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		return 0.0;
+
+	}
+	
+
+	public static void main(String[] args) {
+		
+		try {
+			URL url = new URL(
+					"https://api.getgeoapi.com/v2/currency/convert?api_key=02d64c36ab1702f7c2a39d8001dea0017409f3ae&from="
+							+ "USD" + "&to=" + "ARS" + "&amount=" + "200" + "&format=json");
+
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+			conn.setRequestMethod("GET");
+			conn.connect();
+			int responseCode = conn.getResponseCode();
+
+			if (responseCode != 200) {
+				throw new RuntimeException("Ocurrio un error " + responseCode);
+			} else {
+				StringBuilder informationString = new StringBuilder();
+				Scanner scanner = new Scanner(url.openStream());
+
+				while (scanner.hasNext()) {
+					informationString.append(scanner.nextLine());
+				}
+
+				scanner.close();
+				//System.out.print(informationString);
+				JSONObject jsonObject = new JSONObject(informationString.toString());	
+				
+				System.out.println(jsonObject.getJSONObject("rates").getJSONObject("ARS").getDouble("rate"));
+				
+				
+
+			}
+			
+
+		} catch (Exception err) {
+			err.printStackTrace();
+		}
+		
+
+//		try {
+//			URL url = new URL(
+//					" https://api.getgeoapi.com/v2/currency/historical/2018-10-12?api_key=02d64c36ab1702f7c2a39d8001dea0017409f3ae");
+//			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//			conn.setRequestMethod("GET");
+//			conn.connect();
+//			int responseCode = conn.getResponseCode();
+//
+//			if (responseCode != 200) {
+//				throw new RuntimeException("Ocurrio un error" + responseCode);
+//			} else {
+//				StringBuilder informationString = new StringBuilder();
+//				Scanner scanner = new Scanner(url.openStream());
+//
+//				while (scanner.hasNext()) {
+//					informationString.append(scanner.nextLine());
+//				}
+//
+//				scanner.close();
+//
+//				System.out.print(informationString);
+//
+//				JSONObject jsonObject = new JSONObject(informationString.toString());
+//
+//				String jsontest = jsonObject.getString("base_currency_code");
+//
+//				System.out.println(jsontest);
+//
+//			}
+//
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 	}
 }
